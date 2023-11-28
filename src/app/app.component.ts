00605b7cc78ea9a836cc1f0e5dd066e2f1e2224d
hollowamtt <hollowmatt@gmail.com>
2023-11-28 20:45:00 +0000

    initial setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,12 @@
 import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  imports: [],
+  template: `<h1>Default</h1>`,
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'firstly';
+  title = 'default';
 }
