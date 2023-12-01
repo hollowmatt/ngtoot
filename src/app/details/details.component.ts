a9f272443480fe18abfc3483e0b7cddc2b28fc43
hollowamtt <hollowmatt@gmail.com>
2023-12-01 02:15:16 +0000

    pull data into the details component

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,5 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from '../housing.service';
+import { HousingLocation } from '../housinglocation';
+
 
 @Component({
   selector: 'app-details',
@@ -9,5 +13,9 @@ import { CommonModule } from '@angular/common';
   styleUrl: './details.component.css'
 })
 export class DetailsComponent {
-
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = -1;
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
 }
